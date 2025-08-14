f86c373c96325b9418a5d0ad9171f9c2de488ee7
Denys Stupakov <stupakov106@gmail.com>
2025-08-14 17:51:17 +0200

    implement movie fetching logic and error handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import './App.css'
 import Search from './components/Search'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movies, setMovies] = useState([])
+  const [isLoading, setisLoading] = useState(false)
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies')
+      }
+
+      const data = await response.json()
+
+      if(data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+
+      }
+    } catch (error) {
+      setErrorMessage('Failed to fetch movies. Please try again later.')
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
 
   return (
     <>
@@ -14,9 +54,14 @@ function App() {
           <header>
             <img src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <section className="all-movies">
+            <h2>All movies</h2>
+
+            {errorMessage ? <p className="text-red-500">{errorMessage}</p> : null}
+          </section>
         </div>
       </main>
     </>
